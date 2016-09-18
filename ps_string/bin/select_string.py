09ab2d6431ec0de6cd7269e8aa3e9c80f3b70900
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-09-18 15:55:53 +0000

    modified: ps_string/bin/select_string.py
    new option: --not-match
    
    modified by autopep8

--- ps_string/bin/select_string.py
+++ ps_string/bin/select_string.py
@@ -3,7 +3,8 @@
 
 import argparse
 import os
-import sys,fileinput
+import sys
+import fileinput
 sys.path.insert(0, os.path.abspath('../..'))
 from ps_string import lib
 
@@ -21,9 +22,14 @@ def main():
     parser.add_argument(
         "pattern",
         help="検索する文字列パターン。")
+    parser.add_argument(
+        "--not-match",
+        default=False,
+        action="store_true",
+        help="パターンに合致しない文字列を抽出します。")
     args = parser.parse_args()
-    input_list=fileinput.input(args.path)
-    result = lib.select_string(input_list,args.pattern)
+    input_list = fileinput.input(args.path)
+    result = lib.select_string(input_list, args.pattern, args.not_match)
 
     if len(result) > 0:
         for i in result:
