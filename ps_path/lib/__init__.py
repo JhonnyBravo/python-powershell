8e680c5d84670b3ec9d0d0bc5617ef8f99d6ad7c
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-09-19 15:56:43 +0000

    modified: ps_path/lib/__init__.py
    modified by autopep8.

--- ps_path/lib/__init__.py
+++ ps_path/lib/__init__.py
@@ -1,23 +1,25 @@
 # coding: utf-8
 import os
 
-def test_path(path,path_type="any"):
-    if path_type=="leaf":
-        result=os.path.isfile(path)
-    elif path_type=="container":
-        result=os.path.isdir(path)
-    elif path_type=="any":
-        result=os.path.exists(path)
+
+def test_path(path, path_type="any"):
+    if path_type == "leaf":
+        result = os.path.isfile(path)
+    elif path_type == "container":
+        result = os.path.isdir(path)
+    elif path_type == "any":
+        result = os.path.exists(path)
 
     return result
 
-def split_path(path,leaf,qualifier,parent):
+
+def split_path(path, leaf, qualifier, parent):
     if leaf:
-        result=os.path.basename(path)
+        result = os.path.basename(path)
     elif qualifier:
-        result_list=os.path.splitdrive(path)
-        result=result_list[0]
+        result_list = os.path.splitdrive(path)
+        result = result_list[0]
     elif parent:
-        result=os.path.dirname(path)
+        result = os.path.dirname(path)
 
     return result
