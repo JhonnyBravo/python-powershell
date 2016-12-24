cdd14e8284965fbb2c39cde7a94cfe4fb05737ef
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-12-24 12:34:07 +0900

    modified: ps_path/lib/__init__.py
    ドックストリングスを追加。

--- ps_path/lib/__init__.py
+++ ps_path/lib/__init__.py
@@ -3,6 +3,18 @@ import os
 
 
 def test_path(path, path_type="any"):
+    """
+    指定した path が存在するかどうかを確認します。
+
+    :param str path: 存在を確認するファイル / ディレクトリの path 。
+    :param str path_type:
+        * leaf: 指定した path がファイルである場合に True を返します。
+        * container: 指定した path がディレクトリである場合に True を返します。
+        * any: ファイルであるかディレクトリであるかを問わず、
+          指定した path が存在すれば True を返します。
+    :return: path が存在する場合は True を存在しない場合は False を返します。
+    :rtype: bool
+    """
     if path_type == "leaf":
         result = os.path.isfile(path)
     elif path_type == "container":
@@ -14,6 +26,20 @@ def test_path(path, path_type="any"):
 
 
 def split_path(path, leaf, qualifier, parent):
+    """
+    指定したパスの一部分を取得します。
+
+    :param str path: ファイル / ディレクトリの path 。
+    :param bool leaf: True を指定した場合は path の末尾を取得して返します。
+    :param bool qualifier: True を指定した場合はドライブ名を返します。
+
+        .. note::
+            Windows のみ。
+
+    :param bool parent: True を指定した場合は path の親ディレクトリを返します。
+    :return: パスの一部分。
+    :rtype: str
+    """
     if leaf:
         result = os.path.basename(path)
     elif qualifier:
