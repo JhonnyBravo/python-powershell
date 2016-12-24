7711b5b4ce4c7575ad634fed4e665ac809394e3b
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-12-24 12:33:12 +0900

    modified: ps_content/lib/__init__.py
    ドックストリングスを追加。

--- ps_content/lib/__init__.py
+++ ps_content/lib/__init__.py
@@ -5,6 +5,13 @@ import fileinput
 
 
 def test_path(path):
+    """
+    指定したパスが存在するかどうかを確認します。
+
+    :param str path: 存在を確認する path 。
+        path が存在しない場合、または path がディレクトリである場合は
+        終了ステータス 1 を返して終了します。
+    """
     if not os.path.exists(path):
         print(path + " は存在しません。")
         sys.exit(1)
@@ -14,6 +21,16 @@ def test_path(path):
 
 
 def write_content(path, value, append_text=False):
+    """
+    ファイルへの書込を実行します。
+
+    :param str path: 書込を実行するファイルのパス。
+        ファイルが存在しない場合は新規作成します。
+    :param str value: ファイルへ書き込む値。
+    :param bool append_text: True を指定した場合はファイル末尾へ追記し、
+        False を指定した場合はファイルの内容を上書きします。
+        既定値は False です。
+    """
     if not append_text:
         mode = "wt"
     elif append_text:
@@ -26,6 +43,11 @@ def write_content(path, value, append_text=False):
 
 
 def clear_content(path):
+    """
+    ファイルの内容を削除します。
+
+    :param str path: 内容を削除するファイルのパス。
+    """
     test_path(path)
 
     with open(path, "wt") as file:
@@ -35,6 +57,13 @@ def clear_content(path):
 
 
 def get_content(path):
+    """
+    ファイルの内容を取得し、配列に収めて返します。
+
+    :param str path: 読み込むファイルの path 。
+    :return: 行単位で区切ったファイル内容の配列。
+    :rtype: list
+    """
     test_path(path)
 
     result = []
