645849f64cca29822444b1ac21fd1351d5a36412
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-12-24 12:34:32 +0900

    modified: ps_string/lib/__init__.py
    ドックストリングスを追加。

--- ps_string/lib/__init__.py
+++ ps_string/lib/__init__.py
@@ -5,6 +5,16 @@ import re
 
 
 def select_string(input_list, pattern, not_match=False):
+    """
+    input_list から pattern に合致する文字列を含む行を取得します。
+
+    :param list input_list: 文字列を収めた配列。
+    :param str pattern: pattern に合致する文字列を含む行を取得します。
+    :param bool not_match: True を指定した場合は
+        pattern に合致しない行を取得します。
+    :return: 文字列の配列。
+    :rtype: list
+    """
     result = []
 
     for line in input_list:
